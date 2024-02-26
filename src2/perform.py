287f1e0b9c75d67d7d1509e244fd12a30c48a93a
Edoardo Fazzari <46302551+edofazza@users.noreply.github.com>
2024-02-26 14:16:56 -0600

    Update perform.py

--- src2/perform.py
+++ src2/perform.py
@@ -99,8 +99,10 @@ def train_eval_loop(generator, classifiers, data_loader, input_size_G, knn, k, p
         print(outputs_list[0].size())
         print('TENSOR OUTPUT')
         tensor_outputs = torch.tensor([t.detach().cpu().numpy() for t in outputs_list], device='cuda:0')
+        print(tensor_outputs
+              [0].size())
         tensor_outputs = torch.nn.functional.softmax(tensor_outputs, dim=1)
-        print(tensor_outputs)
+        print(tensor_outputs.shape)
         ensemble_loss = criterion_G(tensor_outputs, labels)
         # Get loss from dynamic ensemble
         #ensemble_loss = torch.mean(torch.tensor(losses))
